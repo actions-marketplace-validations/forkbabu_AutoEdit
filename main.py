4251846dce63ebd682d2c8c9d4582d532ac90a23
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-02 22:57:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from pdflatex import PDFLaTeX
 import os
-path = os.environ["INPUT_PATH"]
-pdfl = PDFLaTeX.from_texfile(path)
-pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=True)
+
+def main():
+  path = os.environ["INPUT_PATH"]
+  pdfl = PDFLaTeX.from_texfile(path)
+  pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=True)
+
+  if __name__ == "__main__":
+    main()
