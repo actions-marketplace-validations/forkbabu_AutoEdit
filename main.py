3cbe02c947d315dac55a5edb6bd03383e87ddbca
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-02 23:40:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from pdflatex import PDFLaTeX
-import os
-
-def main():
-  path = os.environ["INPUT_PATH"]
-  pdfl = PDFLaTeX.from_texfile(path)
-  pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=True)
-
-  if __name__ == "__main__":
-    main()
+pdflatex tests/main.tex
